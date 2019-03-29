a987d462d43c016a8ffc726fea56ee6ba0bb8f97
github username <github email address>
2019-03-29 15:44:07 -0500

    Done.

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,53 @@
+class BoatsController < ApplicationController
+  
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+
+    if @boat.save
+      redirect_to @boat
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+
+    @boat.update(boat_params)
+
+    if @boat.save
+      redirect_to @boat
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    flash[:notice] = "boat deleted."
+    redirect_to boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :length)
+  end
+end
