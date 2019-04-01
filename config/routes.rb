1ebde8e36fa2a5581b5a46e05eb718f89420a8bd
grangerl330 <grangerl330@gmail.com>
2019-03-31 20:41:42 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :boat_classifications, :boats, :captains, :classifications
 end
